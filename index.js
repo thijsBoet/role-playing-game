db17379fd8b67bf981c3a8949310f58dbbfffb5a
thijsBoet <m.boet@chello.nl>
2022-04-25 21:29:42 +0200

    created roll dice function and objects

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-/*
-CHALLENGE
-1. Convert our consts into two objects called 
-"monster" and "hero".
-2. Update the renderCharacter() function so that it accepts 
-a single object "data" as its parameter instead of five string/numbers, 
-reducing the number of arguments to pass in from five to one.
-3. Update the template now each variable is coming from "data".
-4. Update the function call.
-*/
+const renderCharacter = ({
+	elementId,
+	name,
+	avatar,
+	health,
+	diceRoll,
+	diceCount,
+}) => {
+   let diceHtml = '';
 
-const renderCharacter = (data) => {
-   document.getElementById(data.elementId).innerHTML =
+   diceRoll.map(dice => diceHtml += `<div class="dice">${dice}</div>`).join('');
 
-   `<div class="character-card">
-      <h4 class="name"> ${data.name} </h4>
-      <img class="avatar" src="${data.avatar}"/>
-      <p class="health">health: <b> ${data.health} </b></p>
-      <div class="dice-container"><div class="dice"> ${data.diceRoll} </div></div>
+   document.getElementById(elementId).innerHTML = `<div class="character-card">
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}"/>
+      <p class="health">health: <b> ${health} </b></p>
+      <div class="dice-container">
+         ${diceHtml}
+      </div>
    </div>`;
 };
+
+const getDiceRollArray = (diceCount) => {
+   let diceRoll = [];
+   for (let i = 0; i < diceCount; i++) diceRoll.push(Math.floor(Math.random() * 6) + 1);
+   return diceRoll;
+}
+console.log(getDiceRollArray(3));
 
 const hero = {
 	elementId: 'hero',
 	name: 'Wizard',
 	avatar: 'images/wizard.png',
 	health: 60,
-	diceRoll: 6,
+	diceRoll: [3,1,4],
+	diceCount: 3,
 };
 
 const monster = {
@@ -33,7 +41,8 @@ const monster = {
 	name: 'Orc',
 	avatar: 'images/orc.png',
 	health: 10,
-	diceRoll: 4,
+	diceRoll: [2],
+	diceCount: 1,
 };
 
 renderCharacter(hero);
