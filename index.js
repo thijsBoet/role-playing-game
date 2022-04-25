336d7c0119462eb3c469515cc6c3d8d05cc0a2cf
thijsBoet <m.boet@chello.nl>
2022-04-25 22:07:09 +0200

    refactored getDiceRollArray function

--- index.js
+++ index.js
@@ -3,36 +3,34 @@ const renderCharacter = ({
 	name,
 	avatar,
 	health,
-	diceRoll,
 	diceCount,
 }) => {
-   let diceHtml = '';
+	let diceHtml = getDiceHtml(diceCount);
 
-   diceRoll.map(dice => diceHtml += `<div class="dice">${dice}</div>`).join('');
-
-   document.getElementById(elementId).innerHTML = `<div class="character-card">
-      <h4 class="name"> ${name} </h4>
-      <img class="avatar" src="${avatar}"/>
-      <p class="health">health: <b> ${health} </b></p>
-      <div class="dice-container">
-         ${diceHtml}
-      </div>
-   </div>`;
+	document.getElementById(elementId).innerHTML = 
+	`<div class="character-card">
+		<h4 class="name"> ${name} </h4>
+		<img class="avatar" src="${avatar}"/>
+		<p class="health">health: <b> ${health} </b></p>
+		<div class="dice-container">
+			${diceHtml}
+		</div>
+	</div>`;
 };
 
-const getDiceRollArray = (diceCount) => {
-   let diceRoll = [];
-   for (let i = 0; i < diceCount; i++) diceRoll.push(Math.floor(Math.random() * 6) + 1);
-   return diceRoll;
-}
-console.log(getDiceRollArray(3));
+const getDiceRollArray = diceCount => new Array(diceCount).fill(0).map(() => Math.floor(Math.random() * 6) + 1);
+
+const getDiceHtml = diceCount =>
+	getDiceRollArray(diceCount)
+		.map(dice => `<div class="dice">${dice}</div>`)
+		.join('');
+
 
 const hero = {
 	elementId: 'hero',
 	name: 'Wizard',
 	avatar: 'images/wizard.png',
 	health: 60,
-	diceRoll: [3,1,4],
 	diceCount: 3,
 };
 
@@ -41,7 +39,6 @@ const monster = {
 	name: 'Orc',
 	avatar: 'images/orc.png',
 	health: 10,
-	diceRoll: [2],
 	diceCount: 1,
 };
 
